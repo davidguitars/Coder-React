f1e8b8875822aeb0649c253099729ffafea64042
David Urdaneta <davidguitars26@gmail.com>
2022-08-16 23:10:34 -0300

    firebase

--- src/components/ItemListContainer/ItemDetail.jsx
+++ src/components/ItemListContainer/ItemDetail.jsx
@@ -4,28 +4,28 @@ import { useCartContext } from "../Context/CartContext";
 import ItemCount from "../ItemCount";
 import "react-toastify/dist/ReactToastify.css";
 
-export const ItemDetail = ({ products }) => {
+export const ItemDetail = ({ data }) => {
   const [goToCart, setGoToCart] = useState(false);
   const { addToCart } = useCartContext();
 
   const onAdd = (quantity) => {
     setGoToCart(true);
-    addToCart(products, quantity);
+    addToCart(data, quantity);
   };
 
   return (
     <div className="card-container">
       <header className="image-header">
-        <img className="image-main" src={products.image} />
-        <h1>{products.title}</h1>
-        <p className="eth">precio: ${products.price}</p>
-        <h3>Descripcion:{products.description}</h3>
-        <span>stock:{products.stock}</span>
+        <img className="image-main" src={data.image} />
+        <h1>{data.title}</h1>
+        <p className="eth">precio: ${data.price}</p>
+        <h3>Descripcion:{data.description}</h3>
+        <span>stock:{data.stock}</span>
         <div className="boton btn btn-warning">
           {goToCart ? (
             <Link to="/cart">terminar Compra</Link>
           ) : (
-            <ItemCount initial={1} stock={products.stock} onAdd={onAdd} />
+            <ItemCount initial={1} stock={data.stock} onAdd={onAdd} />
           )}
         </div>
       </header>
--- src/components/ItemListContainer/ItemDetailContainer.jsx
+++ src/components/ItemListContainer/ItemDetailContainer.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import {getFirestore, doc, getDoc} from 'firebase/firestore';
-import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 
 
 export default function ItemDetailContainer  ()  {
   const [data, setData] = useState({})
-  const [itemId] = useParams()
+ 
  
  
    useEffect(() => {
@@ -15,7 +14,7 @@ export default function ItemDetailContainer  ()  {
    getDoc(queryDoc)
    .then(res => setData({id: res.data, ...res.data() }))
     
-   }, [itemId]);
+   }, []);
 
   return (
     <div>
