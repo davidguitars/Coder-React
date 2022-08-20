5032449ba2d66fa45cf7d196f77a9e7b1f0a9ff6
David Urdaneta <davidguitars26@gmail.com>
2022-08-20 18:07:28 -0300

    agregando orders

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -1,52 +1,71 @@
 import { useCartContext } from "../components/Context/CartContext";
-import React from "react";
+import { addDoc, getFirestore, collection } from "firebase/firestore";
+
 
 const CartView = () => {
-const { cart, totalProducts, clearCart } = useCartContext();
+  const { cart, totalProducts, clearCart } = useCartContext();
+  const order = {
+    buyers:{
+        name: '',
+        email: '',
+        phone: ''
+    },
+    items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})),
+    total: totalProducts(),
+}
 
-const clearAll = () => {
-  clearCart();
-};
+const terminarCompra = () => {
+    
+   const db = getFirestore()
+   const ordersCollection = collection (db, 'orders')
+   addDoc(ordersCollection, order)
+   .then(({id}) => console.log(id))
+
+
+}
+
+  const clearAll = () => {
+    clearCart();
+  };
 
   if (cart.length === 0) {
     return <h1>No hay articulos en el carrito!</h1>;
   }
 
-
   return (
     <>
-    <div className="cart-container">
-      {cart.map((item) => (
-        <div key={item.id}>
-          <div className="item__card card-in-cart">
-            <div className="item__description card-in-cart-description ">
-            <div className='img__cart'> 
-            <img className='img__cart' src={item.image} alt={item.title}></img>
-            </div>
-              <h5 className="name">{item.title}</h5>
-              <h2 className="price">${item.price}</h2>
-              <ion-icon
-                name="trash-outline"
-                onClick={() => removeProduct(item.id)}
-              ></ion-icon>
-              <p>Subtotal : {item.price * item.quantity}</p>
-              <p>Cantidad: {item.quantity}</p>
+      <div className="cart-container">
+        {cart.map((item) => (
+          <div key={item.id}>
+            <div className="item__card card-in-cart">
+              <div className="item__description card-in-cart-description ">
+                <div className="img__cart">
+                  <img src={item.image} alt={item.title}></img>
+                </div>
+                <h5 className="name">{item.title}</h5>
+                <h2 className="price">${item.price}</h2>
+                <div>   <i onClick={() => removeProduct(item.id)} className="fa-solid fa-trash-can"></i></div>
+                <p>Subtotal : {item.price * item.quantity}</p>
+                <p>Cantidad: {item.quantity}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-    <div className="cart-footer">
-      <div>
-        <p>Total: ${totalProducts()}</p>
-        <button onClick={clearAll} className="btn btn-danger">
-          Vaciar carrito
-        </button>
+        ))}
       </div>
-    </div>
-    
-  </>
-);
+      <div className="cart-footer">
+        <div>
+          <p>Total: ${totalProducts()}</p>
+          <button onClick={terminarCompra} className="btn btn-danger">
+            terminar compra
+          </button>
+
+          <button onClick={clearAll} className="btn btn-danger">
+            Vaciar carrito
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartView;
--- src/components/ItemListContainer/ItemDetailContainer.jsx
+++ src/components/ItemListContainer/ItemDetailContainer.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import {getFirestore, doc, getDoc} from 'firebase/firestore';
 import ItemDetail from "./ItemDetail";
+import {useParams} from 'react-router-dom'
 
 
 export default function ItemDetailContainer  ()  {
   const [data, setData] = useState({})
- 
+ const {itemId} = useParams()
  
  
    useEffect(() => {
    const  querydb = getFirestore()
-   const queryDoc = doc(querydb, 'products', '0DRoNabSFzgqoeY9s786')
+   const queryDoc = doc(querydb, 'products', itemId)
    getDoc(queryDoc)
    .then(res => setData({id: res.data, ...res.data() }))
     
-   }, []);
+   }, [itemId]);
 
   return (
     <div>
